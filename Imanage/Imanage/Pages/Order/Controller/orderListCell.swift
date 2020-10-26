55c8abb33077204caf9972d16dca7d4d1a0e38f8
Aries <37924647+ariesdwi@users.noreply.github.com>
2020-10-26 23:34:01 +0700

    Order front end Done

---
+++ Imanage/Imanage/Pages/Order/Controller/orderListCell.swift
@@ -0,0 +1,26 @@
+//
+//  orderListCell.swift
+//  Imanage
+//
+//  Created by Aries Dwi Prasetiyo on 26/10/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class orderListCell: UITableViewCell {
+
+    @IBOutlet var orderlistImageView: UIImageView!
+    @IBOutlet var orderListName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
